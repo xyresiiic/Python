768eb0aea1f7a3ee9932a97e901ca61254c84ca0
Veer Pratap Singh <xyresiiic@gmail.com>
2026-06-22 07:23:02 +0530

    added program

---
+++ Basics/01_hello_world.py
@@ -0,0 +1,6 @@
+# Program 1: Hello World
+# This is usually the first program anyone writes in a new language.
+# The print() function outputs text to the screen.
+
+print("Hello, World!")
+print("Welcome to Day 1 of learning Python!")
