97b6c3207ade39bfb814f853b26e984ffc287d0b
Veer Pratap Singh <xyresiiic@gmail.com>
2026-06-22 07:27:04 +0530

    added program

---
+++ Basics/03_basic_math.py
@@ -0,0 +1,17 @@
+# This program demonstrates very basic math in Python
+
+# Addition
+sum_result = 5 + 3
+print("5 + 3 =", sum_result)
+
+# Subtraction
+sub_result = 10 - 4
+print("10 - 4 =", sub_result)
+
+# Multiplication
+mult_result = 4 * 2
+print("4 * 2 =", mult_result)
+
+# Division
+div_result = 16 / 2
+print("16 / 2 =", div_result)
