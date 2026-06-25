be5756cc3d899373d0f1607d995e9c82e7b2863e
Veer Pratap Singh <xyresiiic@gmail.com>
2026-06-25 06:44:57 +0530

    update

--- Projects/02_number_guessing.py
+++ Projects/02_number_guessing.py
@@ -1,15 +1,15 @@
 # Basic Project: Number Guessing Game
-# This project combines imports, loops, conditionals, inputs, and variables!
 
 import random
 
-print("Welcome to the Number Guessing Game!")
+print("-----------------------Welcome to the Number Guessing Game---------------------------")
+print("-------------------------------------------------------------------------------------")
 print("I'm thinking of a number between 1 and 100.")
 
-# Generate a random secret number
+# to generate a random number
 secret_number = random.randint(1, 100)
 
-# We'll use a variable to track how many tries the user has taken
+# track tries the user has taken
 attempts = 0
 guessed_correctly = False
 
@@ -17,7 +17,7 @@ guessed_correctly = False
 while not guessed_correctly:
     user_guess_text = input("\nEnter your guess: ")
     
-    # We should handle the error in case the user doesn't type a real number!
+    #in case the user doesn't type a real number!
     try:
         user_guess = int(user_guess_text)
         attempts += 1 # Add 1 to the number of attempts
@@ -32,6 +32,6 @@ while not guessed_correctly:
             guessed_correctly = True # This will stop the while loop
             
     except ValueError:
-        print("Oops! That doesn't look like a valid number. Please try again.")
+        print("That doesn't look like a valid number. Please try again.")
 
-print("\nThanks for playing!")
+print("\nThanks for playing")
