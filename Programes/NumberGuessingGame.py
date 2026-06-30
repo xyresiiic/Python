b324c000cc42db38afdfb163aec856658908ff12
Veer Pratap Singh <xyresiiic@gmail.com>
2026-06-30 07:50:16 +0530

    added program

---
+++ Programes/NumberGuessingGame.py
@@ -0,0 +1,26 @@
+import random
+
+def number_guessing_game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    
+    secret_number = random.randint(1, 100)
+    attempts = 0
+    
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+            
+            if guess < secret_number:
+                print("Too low! Try again.")
+            elif guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Please enter a valid integer.")
+
+if __name__ == "__main__":
+    number_guessing_game()
